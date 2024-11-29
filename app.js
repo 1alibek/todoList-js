03153256192a5230172c40bfc144342a6bd79b82
1alibek <alibekgithub@gmail.com>
2024-11-29 13:49:01 +0500

    done

---
+++ app.js
@@ -0,0 +1,30 @@
+// 1-masala
+// let str = "hello";
+// let res = ""
+// for (let i = 0; i<=str.length-1; i++) {
+// res+=str[i]+ ","
+
+// }
+// console.log(res);
+
+// 3-masala
+// let str = "alibek";
+// let res = ""
+// for (let i = 0; i<=str.length-1; i++) {
+// res+=str[i]+str[i]
+
+// }
+// console.log(res);
+
+let a = "tbc";
+let b = "cbt";
+let c = "";
+for (let i = a.length -1; i >= 0 ; i--) {
+    c += a[i]
+}
+
+if (c === b) {
+    console.log('true');
+} else {
+    console.log('false');
+}
