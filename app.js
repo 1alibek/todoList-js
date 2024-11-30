95e2466552d18a779dcf82ad6a853df960f51dad
1alibek <alibekgithub@gmail.com>
2024-11-30 13:29:32 +0500

    first js done

--- app.js
+++ app.js
@@ -1,44 +1,23 @@
-// 1-masala
-// let str = "hello";
-// let res = ""
-// for (let i = 0; i<=str.length-1; i++) {
-// res+=str[i]+ ","
 
+
+// console.log(b);
+
+// let str = "WIaASWaaSSS";
+// res = "";
+// let count =0
+// for (let i = 0; i < str.length; i++) {
+//   if (str[i]==str[i].toUpperCase()){
+//     res+=str[i]
+//     count+=1
+//   };
 // }
-// console.log(res);
+// console.log(count);
 
-// 3-masala
-// let str = "alibek";
-// let res = ""
-// for (let i = 0; i<=str.length-1; i++) {
-// res+=str[i]+str[i]
-
-// }
-// console.log(res);
-
-// let a = "tbc";
-// let b = "cbt";
-// let c = "";
-// for (let i = a.length -1; i >= 0 ; i--) {
-//     c += a[i]
+// let a = "level";
+// let c = "level";
+// for (let i = 0; i <a.length; i++) {
+//   c += a[i];
 // }
 
-// if (c === b) {
-//     console.log('true');
-// } else {
-//     console.log('false');
-// }
+// console.log(a == c);
 
-// 4-masala
-
-let a = 'abc193'
-let b = 0
-
-for (let i = 0; i < a.length; i++) {
-    let c = a[i]
-    if (c>= '0' && c<= '9') {
-        b += Number(c) 
-    }
-}
-
-console.log(b);
