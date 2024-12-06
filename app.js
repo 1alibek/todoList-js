f3a45d125925172a8e5165ad48bc3e49f5934969
1alibek <alibekgithub@gmail.com>
2024-12-06 13:57:06 +0500

    done 8 task

--- app.js
+++ app.js
@@ -1,88 +1,49 @@
-// let romanToInt = function (s) {
-//   const romanNumber = {
-//     I: 1,
-//     V: 5,
-//     X: 10,
-//     L: 50,
-//     C: 100,
-//     D: 500,
-//     M: 1000,
-//   };
-//   let count = 0;
 
-//   for (let i = 0; i < s.length; i++) {
-//     const current = romanNumber[s[i]];
-//     const next = romanNumber[s[i + 1]];
-//     if (next && current < next) {
-//       console.log(next);
-//       count -= current;
-//     } else {
-//       count += current;
+// foreach------------------------------------------------------
+// Array.prototype.myForEach = function (cb) {
+//   for (let i = 0; i < this.length; i++) {
+//     cb(this[i], i, this);
+//   }
+// };
+// newArr.myForEach((value, idx, arr) => {
+//   console.log(value, idx, arr);
+// });
+// Some---------------------------------------------------------
+// let arr = [1,2,3,4,5,6,7];
+// Array.prototype.mySome = function (cb) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === cb) {
+//       return true;
 //     }
 //   }
- 
-//   return count;
+//   return false;
 // };
+// let newArr = arr.mySome(4);
+// console.log(newArr);
 
-// console.log(romanToInt("IV"));
-// let lengthOfLastWord = function (s) {
-//   let split = s.trim().split(" ");
-//   return split[split.length - 1].length;
-// };
-// let isPalindrome = function (s) {
-//   let str = "";
-
-//   for (let i = 0; i < s.length; i++) {
-//     const char = s[i].toLowerCase();
-//     if ((char >= "a" && char <= "z") || (char >= "0" && char <= "9")) {
-//       str += char;
+// Every--------------------------------------------------
+// let arr = [4,4,8,4];
+// Array.prototype.MyEvery = function (cb) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] !== cb) {
+//       return false;
 //     }
 //   }
-//   return str === str.split("").reverse().join("");
+//   return true;
 // };
-// // console.log(isPalindrome(" "));
+// let newArr = arr.MyEvery(4);
+// console.log(newArr);
 
-// let addDigits = function (num) {
-//   while (num >= 10) {
-//     let sum = 0;
-//     while (num > 0) {
-//       sum += num % 10;
-//       num = Math.floor(num / 10);
-//     }
-//     num = sum;
-//   }
-//   return num;
-// };
-// addDigits(199);
-
-// console.log(obj.name); // bre
-// let lengthOfLastWord = function (s) {
-//   let split = s.trim().split(" ");
-//   return split[split.length - 1].length;
-// };
-// console.log(lengthOfLastWord("   fly me   to   the moon  "));
-// let isPalindrome = function (s) {
-//   let str = "";
-//   let regxp = /[a-z 0-9 A-Z]/;
-//   for (let i = 0; i < s.length; i++) {
-//     if (regxp.test(s[i])) {
-//       str += s[i].toLowerCase();
+// Filter-----------------------------------------------------
+// let arr = [4,4,8,4];
+// Array.prototype.MyFilter = function (cb) {
+//     let res =[]
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === cb) {
+//     res.push(arr[i])
 //     }
 //   }
-//   let str2 = str.split(" ").join("");
-//   return str2 === str2.split("").reverse().join("");
+//   return res;
 // };
-// console.log(isPalindrome("a."));
-// let isPalindrome2 = function (s) {
-//   let str = "";
-
-//   for (let i = 0; i < s.length; i++) {
-//     const char = s[i].toLowerCase();
-//     if ((char >= "a" && char <= "z") || (char >= "0" && char <= "9")) {
-//       str += char;
-//     }
-//   }
-//   console.log(str.split("").reverse().join(""));
-//   return str === str.split("").reverse().join("");
-// };
-// isPalindrome2("A man, a plan, a canal: Panama");
+// let newArr = arr.MyFilter(8);
+// console.log(newArr);
