8cbc4a10930bc398e10d96c64c9f9780db96d230
1alibek <alibekgithub@gmail.com>
2024-12-08 22:35:36 +0500

    done 10 lesson homework

--- app.js
+++ app.js
@@ -1,49 +1,36 @@
 
-// foreach------------------------------------------------------
-// Array.prototype.myForEach = function (cb) {
-//   for (let i = 0; i < this.length; i++) {
-//     cb(this[i], i, this);
-//   }
-// };
-// newArr.myForEach((value, idx, arr) => {
-//   console.log(value, idx, arr);
-// });
-// Some---------------------------------------------------------
-// let arr = [1,2,3,4,5,6,7];
-// Array.prototype.mySome = function (cb) {
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] === cb) {
-//       return true;
-//     }
-//   }
-//   return false;
-// };
-// let newArr = arr.mySome(4);
-// console.log(newArr);
 
-// Every--------------------------------------------------
-// let arr = [4,4,8,4];
-// Array.prototype.MyEvery = function (cb) {
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] !== cb) {
+// 1----------------------------------------------------
+// function arr(arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   const newArr1 = arr1.sort();
+//   const newArr2 = arr2.sort();
+//   for (let i = 0; i < newArr1.length; i++) {
+//     if (newArr1[i] !== newArr2[i]) {
 //       return false;
 //     }
 //   }
 //   return true;
-// };
-// let newArr = arr.MyEvery(4);
-// console.log(newArr);
+// }
+// const array1 = [1, 5, 2, 3];
+// const array2 = [5, 1, 3, 2];
+// console.log(arr(array1, array2));
+// 2------------------------------------------------------------
 
-// Filter-----------------------------------------------------
-// let arr = [4,4,8,4];
-// Array.prototype.MyFilter = function (cb) {
-//     let res =[]
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] === cb) {
-//     res.push(arr[i])
-//     }
+// const array1 = [4, 9, 9, 7, 1];
+
+// let n = 3;
+// let array2 = array1.sort((a, b) => b - a);
+// console.log(array2[n - 1]);
+// 3--------------------------------------------------------------
+
+// let array1 = [9, 9, 9, 7, 7];
+// let res = [];
+// array1.forEach((value) => {
+//   if (!res.includes(value)) {
+//     res.push(value);
 //   }
-//   return res;
-// };
-// let newArr = arr.MyFilter(8);
-// console.log(newArr);
+// });
+// console.log(res.length === 2 ? true : false);
