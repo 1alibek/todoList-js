87dd79f27b99f89407eafdfcd868c207333f7630
1alibek <alibekgithub@gmail.com>
2024-12-10 13:29:26 +0500

    done 11 topic homework

--- app.js
+++ app.js
@@ -1,36 +1,49 @@
-
-
-// 1----------------------------------------------------
-// function arr(arr1, arr2) {
-//   if (arr1.length !== arr2.length) {
-//     return false;
-//   }
-//   const newArr1 = arr1.sort();
-//   const newArr2 = arr2.sort();
-//   for (let i = 0; i < newArr1.length; i++) {
-//     if (newArr1[i] !== newArr2[i]) {
-//       return false;
-//     }
-//   }
-//   return true;
+// 1------------------------------------------------------------
+// function arrayMadness(a, b) {
+//   return (
+//     a.reduce((acc, value) => acc + value ** 2, 0) >
+//     b.reduce((acc1, value1) => acc1 + value1 ** 3, 0)
+//   );
 // }
-// const array1 = [1, 5, 2, 3];
-// const array2 = [5, 1, 3, 2];
-// console.log(arr(array1, array2));
-// 2------------------------------------------------------------
+// console.log(arrayMadness([3],[2]));
+// 2-----------------------------------------------------------------
+// let arr = ["h", "o", "l", "a"];
+// let res = "";
+// for (let i = 0; i < arr.length; i++) {
+//   res += arr[i] + ",";
+// }
+// console.log(res.slice(0,-1));
+// 3--------------------------------------------------------------------
 
-// const array1 = [4, 9, 9, 7, 1];
+// let count = 0;
+// let res = "";
+// let arr = [1, 2, 3, 4];
+// for (let i = 0; i < arr.length; i++) {
+//   count += arr[i];
 
-// let n = 3;
-// let array2 = array1.sort((a, b) => b - a);
-// console.log(array2[n - 1]);
-// 3--------------------------------------------------------------
+//   res = count / arr.length;
+// }
+// console.log(res);
+// 4---------------------------------------------------------------------
+// function stringToArray(string) {
+//   let res = string.split(" ");
+//   return res;
+// }
+// 5------------------------------------------------------------------
+ // let count = 0;
+// let res = "";
+// let arr = [1, 2, 3, 4];
+// for (let i = 0; i < arr.length; i++) {
+//   count += arr[i];
 
-// let array1 = [9, 9, 9, 7, 7];
-// let res = [];
-// array1.forEach((value) => {
-//   if (!res.includes(value)) {
-//     res.push(value);
-//   }
-// });
-// console.log(res.length === 2 ? true : false);
+//   res = count / arr.length;
+// }
+// console.log(res);
+
+// 6-----------------------------------------------------------------------
+
+// function findSmallestInt(arr) {
+//   let min = Math.min(...arr);
+//   return min;
+// }
+// console.log(findSmallestInt([34, 15, 88, 2]));
