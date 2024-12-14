f4000d7d8109ee42fe25a38b4f9b1c0fca5c9b54
1alibek <alibekgithub@gmail.com>
2024-12-14 16:21:51 +0500

    done

--- app.js
+++ app.js
@@ -1,50 +1,14 @@
 // 1-----------------------------------------------------------------
-// function regaxphone(tel) {
-//     const regaxtel = /^\(?\d{3}\)?[- ]?\d{3}-\d{4}$/;
-//     return regaxtel.test(tel);
-// }
-// console.log(regaxphone("123-456-7890"));
-// 2------------------------------------------------------------------
-// function regaxUrl(url) {
-//     let regaxUrl1 = /^(http|https):\/\//i;
-//     return regaxUrl1.test(url)
-// }
-// console.log(regaxUrl("httpss://example.com"));
-// 3-----------------------------------------------------------------------------
-// function regaxUrl(url) {
-//     let regaxUrl1 = /^\d+$/;
-//   return regaxUrl1.test(url)
-// }
-// console.log(regaxUrl("76576677676"));
-// 4-------------------------------------------------------------------------------
-// function regaxtxt(url) {
-//     let regaxtext = /A.*Z|Z.*A/i;
-//   return regaxtext.test(url);
-// }
-// console.log(regaxtxt("AhvfeiZ"));
-// 5-------------------------------------------------------------------------------
-//  function regaxtxt(url) {
-//     let regaxtext = /<\/?[a-zA-Z][^>]*>/
-//   return regaxtext.test(url);
-// }
-// console.log(regaxtxt("<h1>"));
-// 6-------------------------------------------------------
-//  function regaxtxt(url) {
-//     let regaxtext = /^([A-Z]{2})(\d{7})/;
-//   return regaxtext.test(url);
-// }
-// console.log(regaxtxt("AD5412353"));
+// class Person {
+//   constructor(firstName, lastName) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//   }
 
-
-
-
-// xonada bergan masala
-function stars(num) {
-  for (let i = 1; i <= num; i++) {
-    let res = " ".repeat(num - i);
-    let res2 = "*".repeat(i * 2 - 1);
-    console.log(res + res2);
-  }
-}
-
-stars(5);
+//   fullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   }
+// }
+// const fish = new Person("ali", "tursunboyev");
+// console.log(fish.fullName());
+// 2-------------------------------------------------------------------
